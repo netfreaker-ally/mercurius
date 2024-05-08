5d940ada4671e24e305e4e2ca856b57877d72e72
Krishna <hanumaramavath9010@gmail.com>
2024-05-08 16:32:05 +0530

    Created keyAuth Service and added skeleton

---
+++ keyAuth/src/main/java/com/mercurius/keyAuth/KeyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mercurius.keyAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyAuthApplication.class, args);
+	}
+
+}
---
+++ keyAuth/src/test/java/com/mercurius/keyAuth/KeyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercurius.keyAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
