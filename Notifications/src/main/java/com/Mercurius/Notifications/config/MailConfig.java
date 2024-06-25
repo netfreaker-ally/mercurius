b49457adb1c0d62a91dd00675e97de0d898ec1bd
Hanuma Ramavath <hanumaramavath@Hanumas-MacBook-Air.local>
2024-06-25 21:16:49 +0530

    updated noti

--- Notifications/src/main/java/com/Mercurius/Notifications/config/MailConfig.java
+++ Notifications/src/main/java/com/Mercurius/Notifications/config/MailConfig.java
@@ -23,7 +23,7 @@ public class MailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-		 props.put("mail.smtp.ssl.enable", "true"); 
+		 props.put("mail.smtp.ssl.enable", "false"); 
        
         props.put("mail.debug", "true");  
         
